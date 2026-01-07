8b9dfac6e7a9d28b24534196fba39c9d86e49316
Michael Badejo <138082642+Badejomichael@users.noreply.github.com>
2026-01-07 22:20:17 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Sora } from "next/font/google";
+import { Inter, Sora } from "next/font/google"; //google font 
 import "./globals.css";
 
 const inter = Inter({
